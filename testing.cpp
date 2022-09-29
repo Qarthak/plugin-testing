4626f52e6c299bf57885549db8195c428c993998
Qarthak <f20190125@goa.bits-pilani.ac.in>
2022-09-29 22:59:49 +0530

    Saving Progress

--- testing.cpp
+++ testing.cpp
@@ -14,5 +14,5 @@ int main(){
     // cout<<"UI changed..\n";
 	int x;                                                                                        
 	// cin >> x;
-	cout << "10" << endl ;
+	cout << "" << endl ;
 }
